2b024c360125a1ae68ca6e2092cb7e071b0a04d7
JuanDNJ <juandevnjv@gmail.com>
2025-06-27 22:56:46 +0200

    Eliminar archivos CSS no utilizados y reorganizar la estructura de estilos en el proyecto

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '@/assets/styles/index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const MatchDay = createRoot(document.getElementById('root')!);
+
+MatchDay.render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+export default MatchDay
---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+export const TITLE = 'Match Day Prueba';
+export const DESCRIPTION = 'A simple React application to manage match days.';
+export const AUTHORS = [
+  {
+    name: 'JuanDev',
+    url: 'https://github.com/JuanDev'
+  },
+  {
+    name: 'JaneDoe',
+    url: 'https://github.com/JaneDoe'
+  },
+  {
+    name: 'JohnSmith',
+    url: 'https://github.com/JohnSmith'
+  }
+];
