494e5c5efe8cdda1b6c3e95d35ff315f103ad717
JuanDNJ <juandevnjv@gmail.com>
2025-06-28 10:36:55 +0200

    feat: enhance Hero component with new SVG icons and improved styling
    - Added new SVG icons for football player, soccer ball, eye, and space ship.
    - Updated Hero component to include icons in the feature list.
    - Improved styling for hero description and list items in hero.module.css.
    - Adjusted layout and padding for better visual presentation.

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,6 +1,10 @@
 import type { FC } from "react"
 import styles from './hero.module.css'
 import digital2 from "@/assets/images/svg/digital 2.svg"
+import football_player from "@/assets/images/svg/football_player.svg"
+import soccer_ball from "@/assets/images/svg/soccer_ball.svg"
+import space_ship from "@/assets/images/svg/space_ship.svg"
+import eye from "@/assets/images/svg/eye.svg"
 import match_day from "@/assets/images/svg/match_day.svg"
 
 const Hero: FC = () => {
@@ -10,12 +14,32 @@ const Hero: FC = () => {
         <h2 className={styles.heroTitle}>¿Quieres que te vean? Haz que cada partido cuente.</h2>
       </header>
       <article className={styles.heroContent}>
-        <p>Impulsa tu carrera desde hoy.</p>
-        <ul>
-          <li>Crea tu perfil gratis</li>
-          <li>Sube tus estadísticas</li>
-          <li>Hazte visible para clubes</li>
-          <li>Empieza a destacar hoy</li>
+        <p className={styles.heroDescription}>Impulsa tu carrera desde hoy.</p>
+        <ul className={styles.heroList}>
+          <li className={styles.heroListItem}>
+            <i>
+              <img src={football_player} alt="" />
+            </i>
+            <span>Crea tu perfil gratis</span>
+          </li>
+          <li className={styles.heroListItem}>
+            <i>
+              <img src={soccer_ball} alt="" />
+            </i>
+            <span>Sube tus estadísticas</span>
+          </li>
+          <li className={styles.heroListItem}>
+            <i>
+              <img src={eye} alt="" />
+            </i>
+            <span>Hazte visible para clubes</span>
+          </li>
+          <li className={styles.heroListItem}>
+            <i>
+              <img src={space_ship} alt="" />
+            </i>
+            <span>Empieza a destacar hoy</span>
+          </li>
         </ul>
       </article>
       <aside className={styles.groupSvg}>
