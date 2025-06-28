90c9f54c730a1a1cd4e76fe6a632cfeadedb00ad
JuanDNJ <juandevnjv@gmail.com>
2025-06-28 12:55:21 +0200

    Add rounded green icon PNG asset to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Header from '@/components/header/Header'
 import styles from './app.module.css'
 import Hero from './components/hero/Hero'
 import heroSvg from '@/assets/images/svg/Hero.svg'
+import roundedGreenIcon from '@/assets/images/svg/rounded_green_icon.png'
+import portatil from '@/assets/images/svg/Macbook.svg'
 const App: FC = () => {
 
   return (
@@ -13,6 +15,17 @@ const App: FC = () => {
           <Hero />
         </section>
         <article className={styles.article}>
+          <div className={styles.boxSvg}>
+            <div className={styles.boxSvgText}>
+              <i>
+                <img src={roundedGreenIcon} alt="" width={64} />
+              </i>
+              <p className={styles.articleText}>Accede a tus estadísticas personales de cada partido.</p>
+            </div>
+            <div className={styles.boxPortatil}>
+              <img className={styles.portatil} src={portatil} alt="portatil" />
+            </div>
+          </div>
           <img className={styles.articleImage} src={heroSvg} alt="hero" />
         </article>
       </main>
