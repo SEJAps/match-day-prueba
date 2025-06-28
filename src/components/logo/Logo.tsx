7d07ee2426f6467b189331e078aa33272553f19e
JuanDNJ <juandevnjv@gmail.com>
2025-06-28 08:07:09 +0200

    feat: agregar estilos para el componente Logo y corregir la clase de imagen

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,11 +1,11 @@
 import type { FC } from 'react';
 import LogoSvg from '@/assets/images/svg/Logo.svg';
-
+import style from './logo.module.css';
 const Logo: FC = () => {
   return (
-    <h1>
+    <h1 className={style.logo}>
       <a href="/">
-        <img src={LogoSvg} className="logo" alt="Match Day" width={70} height={65} title="Match Day" />
+        <img src={LogoSvg} alt="Match Day" width={70} height={65} title="Match Day" />
       </a>
     </h1>
   );
