ca152e34fae70e2395ce7ca9359129446729ba85
JuanDNJ <juandevnjv@gmail.com>
2025-06-28 12:12:13 +0200

    Refactor code structure for improved readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import type { FC } from 'react'
 import Header from '@/components/header/Header'
 import styles from './app.module.css'
 import Hero from './components/hero/Hero'
-
+import heroSvg from '@/assets/images/svg/Hero.svg'
 const App: FC = () => {
 
   return (
     <>
       <Header />
-      <section className={styles.container}>
-        <main className={styles.main}>
+      <main className={styles.main}>
+        <section className={styles.container}>
           <Hero />
-        </main>
-      </section>
+        </section>
+        <article className={styles.article}>
+          <img className={styles.articleImage} src={heroSvg} alt="hero" />
+        </article>
+      </main>
 
     </>
   )
